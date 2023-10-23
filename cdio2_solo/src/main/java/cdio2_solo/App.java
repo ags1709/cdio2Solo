b4a111c2d950e32647f73cc364e31522a90097ff
Anders Greve Soerensen <anders1709@gmail.com>
2023-10-23 17:18:00 +0200

    Setup maven project

---
+++ cdio2_solo/src/main/java/cdio2_solo/App.java
@@ -0,0 +1,13 @@
+package cdio2_solo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
